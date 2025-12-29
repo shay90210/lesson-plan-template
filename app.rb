c825fa7c9d0c9ab8f940daf298b3ed81ecd1b8b2
Shannon <bentley.shannon@yahoo.com>
2025-12-29 03:24:20 -0600

    added data information for get function

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ get '/' do
 end
 
 get '/' do 
+    @months = {
+        "January" => {
+            title: "Shakespeare: Language, Impulsivity, & Consequence"
+            project: "Character Choices Case Study"
+        },
 
+        "February" => {
+            title: "Voices of Leadership"
+            project: "Rhetorical Strategy Remix"
+        }
+    }
+
+    erb: index
 end
